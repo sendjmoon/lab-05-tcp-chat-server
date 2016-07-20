efcec79e5a8907b1945d170b6f08003878d0796f
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-07-19 19:26:51 -0700

    clientpool.js handles register, data, and end-close event emitters for new client connections

--- lab-james-moon/lib/clientpool.js
+++ lab-james-moon/lib/clientpool.js
@@ -1,3 +1,4 @@
+'use strict';
 const Event = require('events');
 
 const ClientPool = module.exports = function() {
@@ -8,39 +9,30 @@ const ClientPool = module.exports = function() {
     socket.write('welcome\n');
     socket.id = socket.remotePort;
     socket.userName = 'guest-' + socket.id;
+
     socket.on('data', (data) => {
-      // socket.write(socket.userName + ': ' + data.toString());
-      socket.emit('broadcast', data, socket);
+      socket.emit('broadcast', data);
       if (data.toString() === 'END\r\n') {
         socket.emit('end');
       }
     });
+
     socket.on('error', (err) => {
       console.log('error received: ' + err);
     });
-    socket.on('end', () => {
-      console.log('user ' + socket.userName + ' has disconnected');
-      console.log('removing ' + this.pool[socket.id]);
+
+    socket.on('close', () => {
+      console.log(socket.userName + ' has disconnected');
       delete this.pool[socket.id];
       console.log(this.pool);
       socket.end();
     });
+
     socket.on('broadcast', (data) => {
       for(var key in this.pool) {
         this.pool[key].write(socket.userName + ': ' + data.toString());
       }
     });
-
-
-
     this.pool[socket.id] = socket;
-
   });
 };
-
-// on('event tag', (param1, param2, param3) => {
-//   do stuff;
-// });
-//
-//
-// emit('event tag', param1, param2, param3){};
--- lab-james-moon/server.js
+++ lab-james-moon/server.js
@@ -2,10 +2,10 @@ const net = require('net');
 const clientpool = require('./lib/clientpool');
 
 const server = net.createServer();
-const test = new clientpool();
+const newUser = new clientpool();
 
 server.on('connection', (socket) => {
-  test.ee.emit('register', socket);
+  newUser.ee.emit('register', socket);
 });
 
 server.listen(3000, function() {
